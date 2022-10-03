b8cfa760406998cd12f698289ded6dd0956b6183
Swag <john.ifert.miller@gmail.com>
2022-10-02 20:51:15 -0400

    made system function

--- script.js
+++ script.js
@@ -31,21 +31,64 @@ function writePassword() {
     alert("Password must be between 8 and 128 characters");
   } else {  useLetters = confirm("Use lower case letters?");
             useUppers = confirm("Use upper case letters?");
-            useNumbers = confirm("Use numbers");
+            useNumbers = confirm("Use numbers?");
             useSpecial = confirm("Use special characters?");
             if (!useUppers && !useUppers && !useNumbers && !useSpecial) {
               alert("You must choose at least one criteria!");
               return;
-            }
-  for(var i = 0; i < length; i++){
-    password += generatePassword(useLetters, useUppers, useNumbers, useSpecial)
-    // if else
+              }
   }
-}
+  // Created an array to make sure each required field is represented in the passwords length.
+  var charTypesInPassword = Array(length)
+  if (useLetters) {
+    var index = Math.floor(Math.random()*length)
+    charTypesInPassword[index] = 0
+  }
+  if (useUppers) {
+    while(true) {
+      var index = Math.floor(Math.random()*length)
+      if (charTypesInPassword[index] == undefined) {
+        charTypesInPassword[index] = 1
+        break;
+      }
+    }
+  }
+  if (useNumbers) {
+    while(true) {
+      var index = Math.floor(Math.random()*length)
+      if (charTypesInPassword[index] == undefined) {
+        charTypesInPassword[index] = 2
+        break;
+      }
+    }
+  }
+  if (useSpecial) {
+    while(true) {
+      var index = Math.floor(Math.random()*length)
+      if (charTypesInPassword[index] == undefined) {
+        charTypesInPassword[index] = 3
+        break;
+      }
+    }
+  }
+  // Generates the password for user
+  for(var i = 0; i < length; i++){
+    if(charTypesInPassword[i] == undefined) {
+      password += generatePassword(useLetters, useUppers, useNumbers, useSpecial)
+    } else if(charTypesInPassword[i] == 0) {
+      password += generatePassword(true, false, false, false)
+    } else if(charTypesInPassword[i] == 1) {
+      password += generatePassword(false, true, false, false)
+    } else if(charTypesInPassword[i] == 2) {
+      password += generatePassword(false, false, true, false)
+    } else if(charTypesInPassword[i] == 3) {
+      password += generatePassword(false, false, false, true)
+    }
+  }
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   password = "";
+  console.log(charTypesInPassword)
 };
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
